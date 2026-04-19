6b8e3fde27d9e4f8263fd09815526f76912d35e2
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-19 01:58:45 -0500

    completed variables for step 1 decided to against using system input to stay with instructions

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+
+        // given system
+        // Question 1:
+        //declare variables
+        //code solution
+        //use sout display
+        // sout "the answer is " + answer
+
+
+        // notes:
+            // code legible and meaningful
+            // comment and line spacing to sep each question
+            // meaningful comment name and messages so user has all context
+
+        //repeat next exercise
+
+
+
+        // Display Question 1:
+        System.out.println("Question 1: ");
+
+        // create variable for bob salary in float bobSalary
+       float bobSalary = 82000;
+
+
+        // create variable for gary salary in float garySalary
+        float garySalary = 67500;
+
+        // create variable called highestSalary
+        float highestSalary = (bobSalary + garySalary) / 2;
+
+        // do math using Math.Max to determines who's salary is higher; stored in highestSalary
+        // average = (a + b) / 2
+
+        //intiial salary varaiables any value using system input and output
+
+        //display highestSalary using "The highest salary is " + highestSalary
+
+    }
 }
